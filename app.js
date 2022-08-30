1f904f800d8f2e686685d5cb7223aeeb65bbd9c1
blueglass <cardanoviette123@gmail.com>
2022-08-30 01:25:38 -0500

    index styles and app updated

--- app.js
+++ app.js
@@ -1,32 +1,46 @@
 
 const App = () => {
 
-  const [expression, setExpression] = React.useState("")
+  const [expression, setExpression] = React.useState("0")
   const [answer, setAnswer] = React.useState(0)
-  const [allowZeros, setAllowZeros] = React.useState(1)
+  const [allowZeros, setAllowZeros] = React.useState(0)
+  const [allowPoint, SetAllowPoint] = React.useState(1)
 
   const disp = (symbol) => {
     setExpression((prev) => {
       let newVal
       if(/[+*-/]/.test(symbol) && /[+*-/]/.test(prev[prev.length-1])) {
         if(/[-]/.test(symbol)) newVal = prev + symbol
-        else newVal = prev.slice(0,-1) + symbol
+        else {
+          let cnt = 0
+          for(let i=prev.length-1; i>=0; i--){
+            if(/[+*-/]/.test(prev[i])) cnt++
+            else break
+          }
+
+          newVal = prev.slice(0,prev.length-cnt) + symbol
+        }
       }else if(symbol === "0") {
         if(allowZeros === 0) newVal = prev
         else if(allowZeros === 1) {
           newVal = prev + symbol
           setAllowZeros(0)
         }else newVal = prev + symbol
+      }else if(symbol === "."){
+        if(allowPoint === 1){
+          newVal = prev + symbol
+          SetAllowPoint(0)
+          setAllowZeros(2)
+        }else newVal = prev 
       }else {
-        let lastSymbol = prev[prev.length-1]
-
-        if(allowZeros === 0 && /[1-9]/.test(symbol)) newVal = prev.slice(0,-1) + symbol
-        else {
-          if(/[+*-/]/.test(symbol)) setAllowZeros(1)
-          else if(/[1-9.]/.test(symbol)) setAllowZeros(2)
-
-          if(/\./.test(lastSymbol) && symbol === ".") symbol = ""
-
+        if(allowZeros === 0 && /[1-9]/.test(symbol)) {
+          newVal = prev.slice(0,-1) + symbol
+          setAllowZeros(2)
+        }else {
+          if(/[+*-/]/.test(symbol)) {
+            setAllowZeros(1)
+            SetAllowPoint(1)
+          }else setAllowZeros(2)
           newVal = (prev + symbol)
         }
       }
@@ -35,23 +49,28 @@ const App = () => {
   }
 
   const allClear = () => {
-    setExpression("")
+    setAllowZeros(0)
+    SetAllowPoint(1)
+    setExpression("0")
     setAnswer(0)
-    setAllowZeros(1)
   }
 
   const calculate = () => {
-    setAnswer(eval(expression))
-    setExpression(eval(expression))
-    if(answer === "0") setAllowZeros(0)
+    let res = eval(expression)
+    console.log(res)
+    setAnswer(res)
+    setExpression((res).toString())
+    if(res === 0) setAllowZeros(0)
     else setAllowZeros(2)
+    if(Number.isInteger(res)) SetAllowPoint(1)
+    else SetAllowPoint(0)
   }
 
   return (
     <div className="container-fluid justify-content-center">
       <div className="grid justify-content-center mt-5">
-        <div id="display">
-          <input type="text" className="exp text-white border-0 col-12 text-end h4" style={{backgroundColor: 'black'}}value={expression} placeholder="0" disabled />
+        <div id="display-box">
+          <input id="display" type="text" className="exp text-white border-0 col-12 text-end h4" style={{backgroundColor: 'black'}}value={expression} placeholder="0" disabled />
           <input type="text" className="ans text-info border-0 col-12 text-end h2" style={{backgroundColor: 'black'}}value={answer} disabled />
         </div>
         <div onClick={allClear} className="padButton bg-danger" id="clear">AC</div>
